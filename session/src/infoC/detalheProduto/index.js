66875e4f24706f64cd00a212dc91d72841f5f649
JojoLindo-C24 <ra52702356842@acaonsfatima.org.br>
2021-10-07 12:28:45 -0300

    07-10

--- session/src/infoC/detalheProduto/index.js
+++ session/src/infoC/detalheProduto/index.js
@@ -11,6 +11,7 @@ export default function DetalheProduto(props) {
 
     // Array = é uma estrutura de dados que armazena uma coleção de elementos de tal forma que cada um dos elementos possa ser identificado por, pelo menos, um índice ou uma chave. Essa estrutura de dados também é conhecida como variável indexada, vetor e matriz.
     // O método JSON. parse() analisa uma string JSON, construindo o valor ou um objeto JavaScript descrito pela string. Uma função reviver opcional pode ser fornecida para executar uma transformação no objeto resultante antes de ser retornada.
+    // JSON ou JavaScript Object Notation, é um modelo para armazenamento e transmissão de informações no formato texto e que é bastante utilizado por aplicações Web que trabalham com a tecnologia AJAX.
 
     // function comprar() {
     //     let carrinho = Cookies.get('carrinho');
